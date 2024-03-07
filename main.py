86295ed006f525cbab46c9bdc118ec1ed453f9ee
Pravin Tiwari <tiwaripravin114@gmail.com>
2024-03-07 17:20:42 +0530

    Dev-PT:Initialize Flask project and setup project structure

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-# file set-up for the project
+# Importing Libraries
+from flask import Flask
+
+# Setting up the Flask application
+app = Flask(__name__)
+
+# Creating a route for the default URL
+@app.route("/")
+def index():
+    return "Hello World!"
+
+# Running the Flask application
+if __name__ == "__main__":
+    app.run(debug=True)
